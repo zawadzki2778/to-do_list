a58d71249a855529ad14c58d921bb89b7968d4fa
Maksim Zawadzki <zawadzki2778@gmail.com>
2022-10-10 20:17:21 +0300

    realized deleting completed tasks

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const list = document.querySelector(".todolist");
-const items = document.querySelectorAll(".todolist-item"); //записываем все элементы, которые находятся внутри list
+// const items = list.querySelectorAll(".todolist-item"); //записываем все элементы, которые находятся внутри list
+const items = list.children; // чтобы получить динамическую коллекцию которая будет пропадать из разметки переписываемс помощью children
+
+const switchEmptyList = function () {
+  console.log(items);
+  if (items.length === 0) {
+    // добавляем проверку что длина коллекции равна нулю
+    console.log("Все задачи выполнены!");
+  }
+};
 
 const addCheckHandler = function (item) {
   const checkbox = item.querySelector(".todolist-input");
   checkbox.addEventListener("change", function () {
     //вешаем обработчик change который отслеживает изменения checkbox
-    console.log("проверка");
+    item.remove(); // удаляем выполненную задачу (checkbox) при клике
+    switchEmptyList();
   });
 };
 
