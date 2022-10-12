4e20662eb538984cdc3b52fdc1ae01be548089c8
Maksim Zawadzki <zawadzki2778@gmail.com>
2022-10-12 08:55:13 +0300

    added comments to the code

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ newItemForm.addEventListener("submit", function (evt) {
   console.log(taskText);
   const task = newItemTemplate.cloneNode(true)//клонируем шаблон
   //добавляем новую задачу c отрисовкой на странице
-  var taskDescription = task.querySelector('span');
+  const taskDescription = task.querySelector('span');
   taskDescription.textContent = taskText;
   list.appendChild(task);
   console.log(task);
