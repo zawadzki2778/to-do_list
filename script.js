a879eb6d6998718d891d1d08fc1938a7c267feef
Maksim Zawadzki <zawadzki2778@gmail.com>
2022-10-12 14:54:48 +0300

    made deleting completed tasks and text from input, corrected errors

--- script.js
+++ script.js
@@ -5,16 +5,14 @@ const emptyList = document.querySelector(".empty-tasks");
 const newItemForm = document.querySelector(".form-add"); //нашли элемент с классом для добавления формы (новой задачи)
 const newItemTitle = newItemForm.querySelector('.form-add-input');
 const templateTask = document.querySelector('#template-task').content;//достаём шаблон и записываем его в переменную (шаблон не виден при отрисовке)
-console.log(templateTask); 
 const newItemTemplate = templateTask.querySelector('.todolist-item');
-console.log(newItemTemplate);
 
 const switchEmptyList = function () {
-  console.log(items);
   // добавляем проверку что длина коллекции равна нулю
   if (items.length === 0) {
-    emptyList.classList.remove("hidden"); // удаляем класс hidden чтобы скрыть элемент
-    console.log("Все задачи выполнены!");
+    emptyList.classList.remove("hidden"); // удаляем класс hidden чтобы показать элемент
+  } else {
+    emptyList.classList.add('hidden'); // добавляем ... что бы скрыть 
   }
 };
 
@@ -34,11 +32,13 @@ for (let i = 0; i < items.length; i++) {
 newItemForm.addEventListener("submit", function (evt) {
   evt.preventDefault();//отменяем отправку формы по умолчанию
   const taskText = newItemTitle.value;
-  console.log(taskText);
   const task = newItemTemplate.cloneNode(true)//клонируем шаблон
   //добавляем новую задачу c отрисовкой на странице
   const taskDescription = task.querySelector('span');
-  taskDescription.textContent = taskText;
-  list.appendChild(task);
-  console.log(task);
+  taskDescription.textContent = taskText; //устанавливаем тексовое содержимое новой задачи
+  addCheckHandler(task);// удаление новой задачи - передаём task в функцию addCheckHandler до того, как будем добавлять этот элемент на страницу. 
+  list.appendChild(task);//добавляем нов.задачу в конец списка list 
+  switchEmptyList();
+  newItemTitle.value = ''; // сбрасываем текст в поле ввода
+
 });
